bddcedc9c1b44cdf52aba1d8238e4e44a395d626
GerGG <germain.gg90@gmail.com>
2024-03-27 12:22:38 -0400

    shadcn theme and modenight added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ModeToggle } from "./components/ModeToogle";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="h-screen bg-background text-foreground">
+        <div className="flex flex-col py-3 px-3">
+          <div className="flex justify-end">
+            <ModeToggle />
+          </div>
+          <div className="container mx-auto">
+            <h1>TaskList</h1>
+            <h1>TaskForm</h1>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
